23e5ad3513b7219983bce471fb39c3572580c420
mgwedd <michael.wedd@leverege.com>
2019-07-07 23:38:42 -0400

    updated configs

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT : process.env.PORT || 8000,
   NODE_ENV : process.env.NODE_ENV || 'development',
-  API_KEY : '1db06954-6279-447e-89aa-4dd7173ae4d4',
+  API_KEY : process.env.API_KEY || '1db06954-6279-447e-89aa-4dd7173ae4d4',
   DB_URL : process.env.DB_URL || 'postgresql://postgres@localhost/noteful',
   TEST_DB_URL : process.env.TEST_DB_URL || 'postgresql://postgres@localhost/noteful_test'
 }
