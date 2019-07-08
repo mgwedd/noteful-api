4b4e1a835202610e70dfa330582ef9e3141388a4
mgwedd <michael.wedd@leverege.com>
2019-07-08 00:03:31 -0400

    added config for heroku db default var

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT : process.env.PORT || 8000,
   NODE_ENV : process.env.NODE_ENV,
   API_KEY : process.env.API_KEY,
-  DB_URL : process.env.DB_URL,
+  DB_URL : process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful',
   TEST_DB_URL : process.env.TEST_DB_URL
 }
